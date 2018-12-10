220e6cff1bffc119229d65f560d34536eb825e09
treadnought <treadnought@rancoisse.net>
2018-12-10 17:45:37 +1100

    Remove migration from program.cs

--- VoxEvents.API/Program.cs
+++ VoxEvents.API/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace VoxEvents.API
@@ -14,11 +15,23 @@ namespace VoxEvents.API
     {
         public static void Main(string[] args)
         {
+            //var host = BuildWebHost(args);
+            //MigrateDatabase(host);
+            //host.Run();
             BuildWebHost(args).Run();
         }
 
+        //public static void MigrateDatabase(IWebHost host)
+        //{
+        //    using (var scope = host.Services.CreateScope())
+        //    {
+        //        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        //    }
+        //}
+
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
